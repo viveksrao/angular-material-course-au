e1f87b86b9715a11b87f1973565e4bfd1a516b1a
Vivekanand <vivekanand.rao@gmail.com>
2018-03-18 10:57:17 -0700

    Added courses/:id route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { CourseComponent } from './course/course.component';
+import { CourseResolver } from './services/course.resolver';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
+  { path: "courses/:id", component: CourseComponent, resolve: { course: CourseResolver} },
   { path: "**", redirectTo: '/' }
 ];
 
