2c31520df3363fef02dff192f1b5940d98d4bb27
Vivekanand <vivekanand.rao@gmail.com>
2018-03-18 20:52:16 -0700

    Angular Material Data Source - Loading Data From The Backend.

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -20,14 +20,25 @@ export class CoursesService {
 
   findAllCourseLessons(courseId:number): Observable<Lesson[]>{
     return this.http.get('/api/lessons', {
-      params: new HttpParams().set('courseId', courseId.toString()).set('pageNumber', "0").set('pageSize', "1000")
-    }).pipe(map(res => res["payload"]));
+      params: new HttpParams()
+        .set('courseId', courseId.toString())
+        .set('pageNumber', "0")
+        .set('pageSize', "1000")
+    }).pipe(
+      map(res => res["payload"])
+    );
   }
 
   findLessons(courseId:number, filter = '', sortOrder = 'asc', pageNumber = 0, pageSize = 3): Observable<Lesson[]>{
     return this.http.get('/api/lessons', {
-      params: new HttpParams().set('courseId', courseId.toString()).set('filter', filter).set('sortOrder', sortOrder).set('pageNumber', pageNumber.toString()).set('pageSize', pageSize.toString())
-    }).pipe(map(res => res["payload"]));
+      params: new HttpParams()
+        .set('courseId', courseId.toString())
+        .set('filter', filter).set('sortOrder', sortOrder)
+        .set('pageNumber', pageNumber.toString())
+        .set('pageSize', pageSize.toString())
+    }).pipe(
+      map(res => res["payload"])
+    );
   }
 
 }
--- src/app/services/lessons.datasource.ts
+++ src/app/services/lessons.datasource.ts
@@ -2,16 +2,23 @@ import { DataSource, CollectionViewer } from '@angular/cdk/collections';
 import { Observable } from 'rxjs/Observable';
 import { Lesson } from '../model/lesson';
 import { CoursesService } from './courses.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 
 export class LessonsDatasource implements DataSource<Lesson>{
 
-  constructor(private coursesService: CoursesService){
+  private lessonsSubject = new BehaviorSubject<Lesson[]>([]);
 
+  constructor(private coursesService: CoursesService){
+  }
+
+  loadLessons(courseId: number, filter: string, sortDirection: string, pageIndex: number, pageSize: number){
+    this.coursesService.findLessons(courseId, filter, sortDirection, pageIndex, pageSize)
+      .subscribe(lessons => this.lessonsSubject.next(lessons));
   }
 
   connect(collectionViewer: CollectionViewer): Observable<Lesson[]>{
-    return undefined;
+    return this.lessonsSubject.asObservable();
   }
   disconnect(collectionViewer: CollectionViewer): void{
   }
