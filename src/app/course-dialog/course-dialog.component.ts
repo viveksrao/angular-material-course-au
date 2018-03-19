69870de02457d0bcec3420673bb2624d26447e64
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-03-19 13:22:42 -0700

    Created CourseDialog Component.

---
+++ src/app/course-dialog/course-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from "@angular/material";
+
+@Component({
+  selector: 'app-course-dialog',
+  templateUrl: './course-dialog.component.html',
+  styleUrls: ['./course-dialog.component.css']
+})
+export class CourseDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  save(){
+  }
+
+  close(){
+  }
+
+}
