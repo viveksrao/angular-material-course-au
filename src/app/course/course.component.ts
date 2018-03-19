cc61af4ec6a3739deab824d1355d39474d69c7ac
Vivekanand <vivekanand.rao@gmail.com>
2018-03-18 19:12:10 -0700

    Material Data Table Up and Running - Data Source and Custom Look and Feel.

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Course } from '../model/course';
 import { ActivatedRoute } from '@angular/router';
 import { CoursesService } from '../services/courses.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-course',
@@ -11,11 +12,14 @@ import { CoursesService } from '../services/courses.service';
 export class CourseComponent implements OnInit, AfterViewInit {
 
   course:Course;
+  dataSource = new MatTableDataSource([]);
+  displayedColumns = ["seqNo", "description", "duration"];
 
   constructor(private route: ActivatedRoute, private coursesService: CoursesService) { }
 
   ngOnInit() {
     this.course = this.route.snapshot.data["course"];
+    this.coursesService.findAllCourseLessons(this.course.id).subscribe(lessons => this.dataSource.data = lessons);
   }
 
   ngAfterViewInit() {
