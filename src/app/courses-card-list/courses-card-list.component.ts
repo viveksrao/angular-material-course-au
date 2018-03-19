d42102fa4917203c9af72add5b35d9fabbe48c59
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-03-19 13:26:44 -0700

    Angular Material Dialog - Passing Input Data.

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Course } from '../model/course';
+import {MatDialog, MatDialogConfig} from "@angular/material";
+import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
 
 @Component({
   selector: 'app-courses-card-list',
@@ -11,9 +13,18 @@ export class CoursesCardListComponent implements OnInit {
   @Input()
   courses: Course[];
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  editCourse({description, longDescription, category}: Course){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.data = {
+      description, longDescription, category
+    };
+    this.dialog.open(CourseDialogComponent, dialogConfig);
   }
 
 }
