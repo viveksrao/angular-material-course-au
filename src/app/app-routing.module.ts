9bec66514a817be8ee3ad6218fa23232461f622a
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-04-06 11:15:41 -0700

    import of Author Component & addition of authors route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { CourseComponent } from './course/course.component';
 import { CourseResolver } from './services/course.resolver';
+import { AuthorComponent } from './author/author.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "courses/:id", component: CourseComponent, resolve: { course: CourseResolver} },
+  { path: "authors", component: AuthorComponent },
   { path: "**", redirectTo: '/' }
 ];
 
